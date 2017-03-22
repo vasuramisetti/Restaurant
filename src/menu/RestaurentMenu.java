e776181e927d1cc4c92d3904cf770945513160ff
vasudev <vasu123689@gmail.com>
2017-03-21 16:27:10 -0800

    code

--- src/menu/RestaurentMenu.java
+++ src/menu/RestaurentMenu.java
@@ -2,6 +2,7 @@ package menu;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import menu.Item;
 import menu.Menu;
@@ -21,17 +22,19 @@ public class RestaurentMenu {
 		ArrayList<Item> items = getitem.returnitems();
 		
 		
-		
-		
 		Menu breakfast = new Menu();
 		Menu lunch = new Menu();
 		Menu dinner = new Menu();
+		
+		
+		
+	
+		
 		
 		for(Item item: items){
 			if (item.getCategory() == "starters") {
 				breakfast.addItem(item);
 				
-				System.out.println("Hai"+item.getCategory());
 				
 			}
 				 else if (item.getCategory() == "tiffen"){
@@ -39,43 +42,28 @@ public class RestaurentMenu {
 				 }
 				 else  {
 			}
-			System.out.println(item.getCategory() +item.getName() +item.getPrice() +item.getType());
 			
 			}
+		
+		
+		
+		
+		
+		
+	List<Item> items2 = breakfast.getItems();
 	
+	for(Item item2: items2){
+		
+		System.out.println(item2.getName());
 			
-
 		
-
-		
-		
-		
-		
-		/*breakfast.addItem(item3);
-		breakfast.addItem(item4);
-		breakfast.addItem(item5);
-		breakfast.setStartTime(new Date());
-		breakfast.setEndTime(new Date());
-		lunch.addItem(item6);
-		lunch.addItem(item7);
-		lunch.addItem(item8);
-		lunch.addItem(item1);
-		lunch.addItem(item2);
-		lunch.setStartTime(new Date());
-		dinner.addItem(item6);
-		dinner.addItem(item7);
-		dinner.addItem(item8);
-		
-		// System.out.println("Restaurant Name:"+ details.getName() +"\n"
-		// +"Address:"+ details.getAddress() + "\n"+"Phone Number:"+
-		// +details.getPhoneNumberL() +"\n"+"Email:" +details.getEmail() +"\n"
-		// +"Website Name:"+ details.getWebSiteName());
-		// System.out.println("***************");
-		// System.out.println("Starters:");*/
-		//items.addAll(breakfast.getItems());
-		//items.addAll(lunch.getItems());
-
-
 		
 	}
+		
+
+	
+	
+		
+	}
+
 }
